d2776969d8325873b688f40ab3bd9a774ab26469
Sui Gn <natsynapses@gmail.com>
2023-12-17 21:59:48 -0600

    -
    -

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-// server.js
-import express from 'express';
-import { subdomainHandler } from './handlers/subdomainHandler.js';
-
-/**
- * Initializes and starts the Express server.
- * @module server
- */
-
-// Initialize express app
-const app = express();
-
-// Use subdomain handler
-app.use(subdomainHandler);
-
-// Starting the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//index.js
