8df4c86f98c712964729f514b4b3393f4060cb86
Sui Gn <natsynapses@gmail.com>
2024-01-09 17:58:50 -0600

    -
    -

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-var express = require('express');
-var app = express();
-var http = require('http');
-var httpServer = http.createServer(app);
-var https, httpsServer;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Use the environment's port if available, else 3000
 
-// Verifica si estamos en producción y carga los certificados si es necesario
-if (process.env.NODE_ENV === 'production' && process.env.SSL_KEY_PATH && process.env.SSL_CERT_PATH) {
-    var fs = require('fs');
-    https = require('https');
-    var privateKey  = fs.readFileSync(process.env.SSL_KEY_PATH, 'utf8');
-    var certificate = fs.readFileSync(process.env.SSL_CERT_PATH, 'utf8');
-    var credentials = {key: privateKey, cert: certificate};
-    httpsServer = https.createServer(credentials, app);
-}
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-// Iniciar servidores
-if (httpsServer) {
-    httpsServer.listen(443, function(){
-        console.log('Server is listening on https://localhost:443');
-    });
-}
-httpServer.listen(81, function(){
-    console.log('Server is listening on http://localhost:80');
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
